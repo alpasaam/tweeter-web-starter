18b6cf2184e0a161a567ff6facbdbe481f1e0a74
saam naeini <alpasaam422@gmail.com>
2025-10-10 04:13:41 -0600

    NavigateToUserHook Presenter

--- tweeter-web/src/components/NavigateToUserHook.ts
+++ tweeter-web/src/components/NavigateToUserHook.ts
@@ -2,6 +2,11 @@ import { useNavigate } from "react-router-dom";
 import { AuthToken, FakeData, User } from "tweeter-shared";
 import { useMessageActions } from "./toaster/MessageHooks";
 import { useUserInfo, useUserInfoActions } from "./userInfo/UserInfoHooks";
+import {
+  UserHookPresenter,
+  UserHookView,
+} from "../presenter/UserHookPresenter";
+import { useRef } from "react";
 
 export const useUserNavigation = (featurePath: string) => {
   const { displayErrorMessage } = useMessageActions();
@@ -10,36 +15,26 @@ export const useUserNavigation = (featurePath: string) => {
 
   const navigate = useNavigate();
 
+  const listener: UserHookView = {
+    displayErrorMessage,
+    navigate,
+    setDisplayedUser,
+  };
+
+  const presenterRef = useRef<UserHookPresenter | null>(null);
+  if (!presenterRef.current) {
+    presenterRef.current = new UserHookPresenter(listener);
+  }
+
   const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
     event.preventDefault();
 
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const toUser = await getUser(authToken!, alias);
-
-      if (toUser) {
-        if (!toUser.equals(displayedUser!)) {
-          setDisplayedUser(toUser);
-          navigate(`${featurePath}/${toUser.alias}`);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
+    await presenterRef.current?.navigateToUser(
+      event,
+      featurePath,
+      authToken!,
+      displayedUser!
+    );
   };
 
   return { navigateToUser };
