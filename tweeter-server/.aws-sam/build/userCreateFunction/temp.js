a616c510e2fd445e90a049bfd1dc90ece8c14b7f
saam naeini <alpasaam422@gmail.com>
2025-11-13 00:52:18 -0700

    Add initial project structure for tweeter-server including configuration files, package management, and AWS SAM setup

---
+++ tweeter-server/.aws-sam/build/userCreateFunction/temp.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Temp = void 0;
+const Temp = () => {
+    console.log("CS 340 is Awesome!");
+};
+exports.Temp = Temp;
